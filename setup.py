3a2195d9874dbaced0b7cb79e5407b8dab34a0fc
Graham Binns <graham@outcoded.uk>
2017-03-21 10:29:21 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-  name='django-useful-enums',
-  packages=['django-useful-enums'],
+  name='usefulenums',
+  packages=['usefulenums'],
   version='0.1',
   description='A useful enumeration / choice library for Django.',
   author='Graham Binns',
   author_email='graham@outcoded.uk',
-  url='https://github.com/grahambinns/django-useful-enums',
+  url='https://github.com/grahambinns/usefulenums',
   download_url=(
-      'https://github.com/grahambinns/django-useful-enums/archive/0.1.tar.gz'),
+      'https://github.com/grahambinns/usefulenums/archive/0.1.tar.gz'),
   keywords=['django', 'choices', 'enumerations'],
   classifiers=[],
 )
